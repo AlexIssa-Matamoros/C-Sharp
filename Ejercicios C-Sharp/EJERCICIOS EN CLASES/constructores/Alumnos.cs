a7f678cb2b0d3b230280ba84c2a42b584995b319
Alex Matamoros <isai.mat43@gmail.com>
2021-03-13 14:13:36 -0600

    Tareas y Ejercicios

---
+++ Ejercicios C-Sharp/EJERCICIOS EN CLASES/constructores/Alumnos.cs
@@ -0,0 +1,8 @@
+namespace Constructores
+{
+    class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Ejercicios C-Sharp/EJERCICIOS EN CLASES/constructores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.Id = 1;
+            a.Nombre = "Juan";
+
+
+            Console.WriteLine(a.Nombre);
+        }
+    }
+}
